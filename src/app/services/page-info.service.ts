528cc3ba8b5dede8dcaf65ef0216e68a347aa6da
elviac94 <elvi_ac94@hotmail.com>
2020-08-04 08:45:22 +0200

    connect project with Firebase

--- src/app/services/page-info.service.ts
+++ src/app/services/page-info.service.ts
@@ -9,17 +9,33 @@ export class PageInfoService {
 
   dataInfor: InfoPagina = {};
   cargada = false;
+  equipo: any[] =[];
 
   constructor( private http: HttpClient) {
     console.log('Servicio info listo')
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+   }
 
-    // leer el JSON
+   private cargarInfo(){
+     // leer el JSON
     this.http.get('assets/data/info.json')
       .subscribe( (resp: InfoPagina) => {
 
         this.cargada = true;
         this.dataInfor = resp;
+      });
+   }
+
+   private cargarEquipo() {
+
+    this.http.get('https://project-angular-c025d.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+
+        this.equipo =resp
         console.log(resp);
       });
+     
    }
 }
